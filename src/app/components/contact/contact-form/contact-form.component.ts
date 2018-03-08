5918a357d59a4367e7b4a52946750d1265c048f4
unknown <sketchyrandom@gmail.com>
2018-03-08 21:12:35 +1100

    Fixed some css bugs and added sticky footer

--- src/app/components/contact/contact-form/contact-form.component.ts
+++ src/app/components/contact/contact-form/contact-form.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseAuthService } from '../../../services/firebase-auth.service';
-declare var require: any;
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-//const admin = require('firebase-admin');
-//admin.initializeApp(functions.config().firebase);
 
 @Component({
   selector: 'app-contact-form',
@@ -21,20 +13,5 @@ export class ContactFormComponent implements OnInit {
   ngOnInit() {
   }
 
-  // sendMessage() {
-  //   if(this.auth.user) {
-  //     const user = this.auth.user;
-  //     console.log('A new user signed in for the first time.');
-  //     const fullName = user.displayName || 'Anonymous';
-    
-  //     // Saves the new welcome message into the database
-  //     // which then displays it in the FriendlyChat clients.
-  //     return admin.database().ref('messages').push({
-  //       name: 'Firebase Bot',
-  //       photoUrl: '/assets/images/firebase-logo.png', // Firebase logo
-  //       text: `${fullName} signed in for the first time! Welcome!`
-  //     });
-  //   }
-  // }
 
 }
